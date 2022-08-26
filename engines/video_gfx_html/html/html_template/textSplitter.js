4b7f95267631a8553f2903704c35694fcfc82d47
chemradio <chemradio@gmail.com>
2022-08-26 12:53:19 +0600

    struggle with text splitting continues

--- engines/video_gfx_html/html/html_template/textSplitter.js
+++ engines/video_gfx_html/html/html_template/textSplitter.js
@@ -1,6 +1,7 @@
 let targetText = 'Украина еще в апреле была признана одной из самых заминированных стран мира. По оценке местной Государственной службы по чрезвычайным ситуациям (ГСЧС), минами и боеприпасами может быть загрязнена почти половина территории страны. Для полного разминирования земель, заминированных к середине августа, по прогнозу МВД Украины, может понадобиться от пяти до 10 лет. Наиболее заминированные области — Киевская и Черниговская; с начала апреля, после отступления российской армии, там обезвредили более 100 тысяч взрывоопасных предметов. По просьбе «Медузы» украинский фотограф Павел Дорогой съездил в Киевскую, Черниговскую и Сумскую области, где ведутся работы по разминированию, — и описал свои впечатления. А украинский журналист Василий Калына поговорил о разминировании Украины с саперами.';
 
-const maxCharsPerLine = 80; 
+const maxCharsPerLine = 20;
+const minCharsPerLine = 20;
 
 let procString = targetText;
 let output = '';
@@ -8,9 +9,19 @@ let output = '';
 while (true) {
     if (procString.length >= maxCharsPerLine) {
         let targetIndex = procString.slice(0,maxCharsPerLine).lastIndexOf(' ');
-        let splitStrings = [procString.slice(0,targetIndex), procString.slice(targetIndex)]
-        output += (splitStrings[0] + '\n')
-        procString = splitStrings[1]
+
+        let firstString = procString.slice(0,targetIndex);
+        let secondString = procString.slice(targetIndex);
+
+        while (true) {
+            let lastWhite = firstString.lastIndexOf(' ');
+            if ( (targetIndex - lastWhite) < 3) {}
+
+        }
+
+        // let splitStrings = [firstString, secondString];
+        output += (firstString + '\n');
+        procString = secondString;
     } else {
         output += procString;
         break
