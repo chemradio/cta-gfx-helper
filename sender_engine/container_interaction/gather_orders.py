d594c5cd25c4edd22424c2f56e4a98b6cdbb2758
chemradio <chemradio@gmail.com>
2022-12-27 01:15:01 +0600

    fix sende request

--- sender_engine/container_interaction/gather_orders.py
+++ sender_engine/container_interaction/gather_orders.py
@@ -6,6 +6,9 @@ from config import GET_ONE_ORDER_ENDPOINT
 
 
 def get_ready_to_send_order() -> Optional[list]:
-    r = requests.get(GET_ONE_ORDER_ENDPOINT, json={"current_stage": "ready_to_send"})
-    result = r.json()
-    return result if result else None
+    try:
+        r = requests.get(GET_ONE_ORDER_ENDPOINT, json={"current_stage": "ready_to_send"})
+        result = r.json()
+        return result
+    except:
+        return None
