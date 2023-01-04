0085f9ee6c48ba4cbd3a896e3198b4a3804b1bf8
chemradio <49790329+chemradio@users.noreply.github.com>
2023-01-04 12:47:27 +0000

    Sender sends only one order. neeed to fix

--- sender_engine/send_process/orders_sender.py
+++ sender_engine/send_process/orders_sender.py
@@ -9,23 +9,32 @@ from container_interaction.gather_orders import get_ready_to_send_order
 from send_process.send_order import send_order
 
 
-async def orders_sender():
+def orders_sender():
     while True:
+
+        print("enter orders sender")
+        print("getting one order")
+
         order = get_ready_to_send_order()
         if not order:
             break
 
-        sent_order = await send_order(order)
+        print(f"order is ok, {order=}")
 
+        sent_order = asyncio.run(send_order(order))
+        print(f"{sent_order=}")
         if not sent_order.get("send_success"):
-            await bot.send_message(BOT_ADMIN, f"Failed to send order\n\n{order}")
+            asyncio.run(bot.send_message(BOT_ADMIN, f"Failed to send order\n\n{order}"))
 
-        mark_success = await mark_order_sent(sent_order)
+        mark_success = mark_order_sent(sent_order)
+        print(f"{mark_success=}")
 
         if not mark_success:
-            await bot.send_message(
-                BOT_ADMIN,
-                f"Failed to mark sent order\n{sent_order.get('send_success')=}\n\n{sent_order=}",
+            asyncio.run(
+                bot.send_message(
+                    BOT_ADMIN,
+                    f"Failed to mark sent order\n{sent_order.get('send_success')=}\n\n{sent_order=}",
+                )
             )
 
         time.sleep(3)
@@ -40,8 +49,8 @@ def orders_sender_thread():
             print(f"Thread {thread_name} already running... Returning")
             return
 
-    threading.Thread(
-        target=asyncio.run,
-        args=(orders_sender(),),
+    thread = threading.Thread(
+        target=orders_sender,
         name=thread_name,
     ).start()
+    thread
