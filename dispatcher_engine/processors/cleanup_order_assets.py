618fa633a5a3791e3cbddf46fe8867f00f2e6220
chemradio <49790329+chemradio@users.noreply.github.com>
2023-01-06 12:33:18 +0000

    more fix cleanup + typo in videoGFX

--- dispatcher_engine/processors/cleanup_order_assets.py
+++ dispatcher_engine/processors/cleanup_order_assets.py
@@ -5,71 +5,98 @@ import config
 
 
 def cleanup_order_assets(order: dict):
-    bg_name = order.get("background_name")
-    fg_name = order.get("foreground_name")
-    audio_name = order.get("audio_name")
-    video_gfx_name = order.get("video_gfx_name")
-    paths = find_files(bg_name, fg_name, audio_name, video_gfx_name)
-    paths = [path for path in paths if path is not None]
-    if not paths:
-        return None
-
-    for path in paths:
-        if path.is_file():
-            path.unlink()
-        elif path.is_dir():
-            path.rmdir()
-
+    # get all possible db filenames
+    bg_name: str = order.get("background_name")
+    fg_name: str = order.get("foreground_name")
+    audio_name: str = order.get("audio_name")
+    video_gfx_name: str = order.get("video_gfx_name")
     html_assembly_name: str = order.get("html_assembly_name")
+
+    # filter out missing filenames
+    temp_filenames = (bg_name, fg_name, audio_name, video_gfx_name)
+    order_filenames = (filename for filename in temp_filenames if filename)
+
+    # add same filenames but without extensions - file type conversion artifacts
+    target_filenames = list(order_filenames)
+    dot_filenames = [filename for filename in target_filenames if "." in filename]
+
+    for filename in dot_filenames:
+        no_ext_filename = filename.split(".")[0]
+        target_filenames.append(no_ext_filename)
+        possible_extensions = (
+            "pdf",
+            "jpg",
+            "jpeg",
+            "wav",
+            "mp3",
+            "ogg",
+        )
+        target_filenames.extend(
+            [f"{no_ext_filename}.{ext}" for ext in possible_extensions]
+        )
+
+    # find target file paths
+    filepaths = find_files(*target_filenames)
+    target_filepaths = (path for path in filepaths if path)
+
+    # remove file paths
+    if not target_filepaths:
+        return None
+    for filepath in target_filepaths:
+        try:
+            filepath.unlink()
+        except:
+            print(f"Failed to remove file: {filepath}")
+
+    # remove html_assembly folder
     if html_assembly_name:
-        html_assembly_path = config.HTML_ASSEMBLIES_FOLDER / html_assembly_name
-        if html_assembly_path.exists():
-            remove_directory_recursively(html_assembly_path)
+        html_path = config.HTML_ASSEMBLIES_FOLDER / html_assembly_name
+        remove_tree(html_path)
+        html_path.rmdir()
 
 
 def find_files(*filenames: list[str]) -> list[Optional[Path]]:
-    print(f"Entered find files. {filenames=}")
-    paths = list()
+    def find_file(file_name: str, search_folders: list[Path]) -> list[Optional[Path]]:
+        output = list()
+        for folder in search_folders:
+            if (folder / file_name).exists():
+                output.append(folder / file_name)
+        return output
+
+    def find_file_glob(
+        file_name: str, search_folders: list[Path]
+    ) -> list[Optional[Path]]:
+        output = list()
+        for folder in search_folders:
+            glob_list = list(folder.glob(f"*{file_name}*"))
+            if not glob_list:
+                continue
+            for item in glob_list:
+                if item.exists():
+                    output.append(folder / file_name)
+        return output
+
+    output_paths = list()
     folders = (
         config.SCREENSHOTS_FOLDER,
         config.USER_FILES_FOLDER,
         config.RENDER_OUTPUT_PATH,
     )
 
-    def find_file(file_name: str, search_folders: list[Path]) -> Optional[list[Path]]:
-        output = list()
-        for folder in search_folders:
-            if (folder / file_name).exists():
-                output.append(folder / file_name)
-            for item in folder.glob(f"{file_name}*"):
-                if item.exists():
-                    output.append(item)
-        return output if output else None
-
     for filename in filenames:
         if not filename:
             continue
 
-        files_to_search = [
-            filename,
-        ]
-        if "." in filename:
-            files_to_search.append(filename.split(".")[0])
-
-        for file in files_to_search:
-            file_to_remove = find_file(file, folders)
-            if file_to_remove:
-                paths.extend(file_to_remove)
-
-    print(f"{paths=}")
-    return paths
+        files_to_remove = find_file(filename, folders)
+        if files_to_remove:
+            output_paths.extend(files_to_remove)
+    return output_paths
 
 
-def remove_directory_recursively(path: Path):
+def remove_tree(path: Path) -> None:
     for item in path.glob("*"):
         if item.is_file():
             item.unlink()
         elif item.is_dir():
-            remove_directory_recursively(item)
+            remove_tree(item)
             item.rmdir()
-    path.rmdir()
