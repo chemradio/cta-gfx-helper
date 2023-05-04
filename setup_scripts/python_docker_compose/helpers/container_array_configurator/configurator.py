ca84622b2c167edcc72b966f397d84f4c3f4ab5b
chemradio <49790329+chemradio@users.noreply.github.com>
2023-05-04 08:05:08 +0000

    setup tools work in progress

--- setup_scripts/python_docker_compose/helpers/container_array_configurator/configurator.py
+++ setup_scripts/python_docker_compose/helpers/container_array_configurator/configurator.py
@@ -1,6 +1,8 @@
 from helpers.container_array_builder.compose_template import (
     UniversalDockerComposeTemplate,
 )
+from helpers.container_array_configurator.volume_template import DockerComposeVolume
+from helpers.enums.enum_selenium_image import SeleniumImage
 
 
 class ComposeConfigurator:
@@ -11,6 +13,10 @@ class ComposeConfigurator:
     def configure(
         cls, user_data: dict, compose_template: UniversalDockerComposeTemplate
     ):
+        # general config
+        compose_template = cls._configure_for_local(user_data, compose_template)
+
+        # platform specific config
         match user_data.get("platform"):
             case "local":
                 return cls._configure_for_local(user_data, compose_template)
@@ -22,19 +28,71 @@ class ComposeConfigurator:
                 return None
 
     @classmethod
-    def _configure_for_azure(
+    def _general_config(
         cls, user_data: dict, compose_template: UniversalDockerComposeTemplate
     ):
+        if user_data.get("specify_container_names"):
+            for container_base_name, container_user_name in user_data["container_names"].items():
+                ...
+
+        if user_data.get("specify_container_ports"):
+            for container_base_name, container_user_port in user_data["container_ports"].items():
+                ...
+
+        # configure architecture / ??? why
         ...
+
+        if user_data.get("is_arm"):
+            compose_template.screenshot_selenium.image = SeleniumImage.ARM
+            compose_template.video_gfx_selenium.image = SeleniumImage.ARM
+
+        # configure rebuild Database
+        compose_template.env_file['rebuild_db'] = True if user_data.get("rebuild_db") else False
+        return compose_template
+
 
     @classmethod
     def _configure_for_local(
         cls, user_data: dict, compose_template: UniversalDockerComposeTemplate
     ):
-        ...
+        # generate local volume
+        compose_template.volume = cls._generate_local_volume()
+
+        # 'mount_code_folders': False,
+        return compose_template
+
+    @classmethod
+    def _configure_for_azure(
+        cls, user_data: dict, compose_template: UniversalDockerComposeTemplate
+    ):
+        compose_template.volume = cls._generate_azure_volume()
+        return compose_template
+
 
     @classmethod
     def _configure_for_codespaces(
         cls, user_data: dict, compose_template: UniversalDockerComposeTemplate
     ):
         return cls._configure_for_local(user_data, compose_template)
+
+
+    @classmethod
+    def _generate_local_volume(cls) -> DockerComposeVolume:
+        return DockerComposeVolume(
+            name="asset_storage",
+            driver= "local",
+            driver_type='none',
+            driver_o='bind',
+            driver_device='$PWD/dev/volume/asset_storage'
+        )
+
+
+    @classmethod
+    def _generate_azure_volume(cls) -> DockerComposeVolume:
+        return DockerComposeVolume(
+            name="asset_storage",
+            driver="azure_file",
+            driver_type='none',
+            driver_o='bind',
+            driver_device='$PWD/dev/volume/asset_storage'
+        )
