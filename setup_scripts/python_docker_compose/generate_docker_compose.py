881bbac160d91ee0768097d693b1f4a8dc7f9d0d
chemradio <49790329+chemradio@users.noreply.github.com>
2023-05-04 08:05:08 +0000

    setup tools work in progress

--- setup_scripts/python_docker_compose/generate_docker_compose.py
+++ setup_scripts/python_docker_compose/generate_docker_compose.py
@@ -2,16 +2,19 @@ from helpers.ask_questions import gather_user_data
 from helpers.container_array_builder.compose_template import (
     UniversalDockerComposeTemplate,
 )
-
+from helpers.container_array_configurator.configurator import ComposeConfigurator
 
 def main():
     # collect user configuration data
     user_data = gather_user_data()
-
+    from pprint import pprint
+    pprint(user_data)
+    return 
     # generate universal compose template
-    universal_template = UniversalDockerComposeTemplate()
+    compose_template = UniversalDockerComposeTemplate()
 
     # configure the template with user submitted data
+    ComposeConfigurator.configure(user_data, compose_template)
 
     # generate dict
     ...
@@ -19,8 +22,10 @@ def main():
     # generate yaml
     ...
 
-    # run docker compose
-    ...
+
+    if user_data.get('run_after_generation'):
+        # run docker compose
+
 
 
 if __name__ == "__main__":
--- setup_scripts/python_docker_compose/helpers/container_array_builder/compose_template.py
+++ setup_scripts/python_docker_compose/helpers/container_array_builder/compose_template.py
@@ -14,16 +14,28 @@ class UniversalDockerComposeTemplate:
         default_images=DefaultContainerImages,
     ):
         self.container_list = container_list
+        self.default_names = default_names
+        self.default_ports = default_ports
+        self.default_images = default_images
+        self.env_file = {}
 
+        self._populate_container_attributes()
+        self._populate_env_file()
+        
+
+    def _populate_container_attributes(self):
         # assign dummy blank objects to container name attributes
-        for container_base_name in container_list:
+        for container_base_name in self.container_list:
             setattr(
                 self,
                 container_base_name,
                 DockerComposeContainer(
                     base_name=container_base_name,
-                    name=getattr(default_names, container_base_name, None),
-                    port=getattr(default_ports, container_base_name, None),
-                    image=getattr(default_images, container_base_name, None),
+                    name=getattr(self.default_names, container_base_name, None),
+                    port=getattr(self.default_ports, container_base_name, None),
+                    image=getattr(self.default_images, container_base_name, None),
                 ),
             )
+
+    def _populate_env_file(self):
+        pass
