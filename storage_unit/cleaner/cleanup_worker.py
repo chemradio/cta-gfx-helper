fa53fb61b79f16c6ce3770225666ea3b4ee0db6f
chemradio <chemradio@gmail.com>
2023-06-14 11:37:05 +0600

    pre main debug

--- storage_unit/cleaner/cleanup_worker.py
+++ storage_unit/cleaner/cleanup_worker.py
@@ -1,7 +1,21 @@
 import time
 
+from cleaner.helpers.asset_eraser import erase_order_assets
+from cleaner.helpers.fetch_orders import fetch_all_active_orders
+from cleaner.helpers.order_classifier import should_order_retire
+
+ORDER_MAX_AGE_HOURS = 7
+
 
 def expiry_asset_cleaner() -> None:
     while True:
-        print("cleaner working every 30 secs.")
-        time.sleep(30)
+        print("cleaner working every hour.")
+        time.sleep(60 * 60)
+
+        active_orders = fetch_all_active_orders()
+        if not active_orders:
+            continue
+
+        for order in active_orders:
+            if should_order_retire(order, ORDER_MAX_AGE_HOURS):
+                erase_order_assets(order)
