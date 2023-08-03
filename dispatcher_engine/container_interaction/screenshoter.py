0817313c957e67e57ae41407a79270b0a9edaf75
chemradio <chemradio@gmail.com>
2023-08-03 18:59:39 +0600

    restored telegram functionality. probably the best bet for self-hosting on Raspberry PI

--- dispatcher_engine/container_interaction/screenshoter.py
+++ dispatcher_engine/container_interaction/screenshoter.py
@@ -3,6 +3,6 @@ import requests
 from config import SCREENSHOTER_ENDPOINT
 
 
-def signal_to_screenshoter(order: dict = {}):
+async def signal_to_screenshoter(order: dict = {}):
     r = requests.post(SCREENSHOTER_ENDPOINT, json=order)
     print(r.json())
