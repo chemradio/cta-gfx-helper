9522415f95ee1ca5b8c51368e3d1986417c114cb
chemradio <chemradio@gmail.com>
2023-08-07 20:43:01 +0600

    video files actually working

--- dispatcher_engine/utils/assets/file_convert/file_convert.py
+++ dispatcher_engine/utils/assets/file_convert/file_convert.py
@@ -1,17 +1,7 @@
-import secrets
-import uuid
-
 from fastapi import UploadFile
-
 from utils.assets.file_convert.workers.audio_to_wav import convert_audio_to_wav
 from utils.assets.file_convert.workers.pdf_to_png import convert_pdf_to_png
 from utils.assets.file_convert.workers.word_to_png import convert_word_to_png
-
-
-def generate_random_filename(prefix: str = "user", extension: str = str()):
-    random_name = str(uuid.uuid4())
-    filename = f"{prefix}_{random_name}.{extension}"
-    return filename
 
 
 class UnconvertableFile(Exception):
@@ -23,24 +13,18 @@ class UnconvertableFile(Exception):
         return f"Unable to convert file: {self.filename}"
 
 
-async def convert_unsupported_file(upload_file: UploadFile) -> tuple[str, bytes]:
+async def convert_unsupported_file(upload_file: UploadFile) -> bytes:
     try:
         file_mime = upload_file.content_type
         if "audio" in file_mime:
-            file_bytes = convert_audio_to_wav(upload_file)
-            extension = "wav"
+            return convert_audio_to_wav(upload_file)
 
         elif file_mime == "application/pdf":
-            file_bytes = convert_pdf_to_png(upload_file)
-            extension = "png"
+            return convert_pdf_to_png(upload_file)
 
         elif file_mime == "application/msword":
             raise UnconvertableFile(upload_file.filename)
             file_bytes = convert_word_to_png(upload_file)
-            extension = "png"
 
     except:
         raise UnconvertableFile(upload_file.filename)
-
-    filename = generate_random_filename(extension=extension)
-    return filename, file_bytes
