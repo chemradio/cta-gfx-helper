73ecdbffe54cab76bf14e407e1f6bbaaa6472325
chemradio <chemradio@gmail.com>
2023-08-31 23:38:10 +0600

    testing performance with chrome options disabled

--- video_gfx/video_gfx/png_extractor_logic/create_driver.py
+++ video_gfx/video_gfx/png_extractor_logic/create_driver.py
@@ -4,12 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 def create_driver(driver_url: str) -> webdriver.Remote:
     chrome_options = Options()
-    chrome_options.add_argument("--allow-file-access-from-files")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.headless = True
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("-–disable-gpu")
+
+    # chrome_options.add_argument("--allow-file-access-from-files")
+    # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    # chrome_options.headless = True
+    # chrome_options.add_argument("--window-size=1920,1080")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("-–disable-gpu")
+
     device_emulation = {
         "deviceMetrics": {
             "width": 1920,
