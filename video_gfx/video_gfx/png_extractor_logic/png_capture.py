dce6bd3356a2f22fac5d889bb29e41423f0cda1c
chemradio <chemradio@gmail.com>
2023-09-02 00:09:52 +0600

    small fixes here and there

--- video_gfx/video_gfx/png_extractor_logic/png_capture.py
+++ video_gfx/video_gfx/png_extractor_logic/png_capture.py
@@ -1,4 +1,9 @@
+import base64
+import json
 import time
+from io import BytesIO
+from pathlib import Path
+from time import perf_counter
 
 from selenium import webdriver
 
@@ -21,9 +26,15 @@ def png_capture(
     print("getting target url", flush=True)
     driver.get(target_url)
     time.sleep(2)
+    print("starting extraction", flush=True)
     for frame in range(start_frame, end_frame + 1):
         progress_frame = linear_interpolation(interpolation_data, frame)
         driver.execute_script(f"timeline.progress({progress_frame})")
+
+        t1 = perf_counter()
         driver.save_screenshot(f"{png_path}/{frame:04}.png")
+        t2 = perf_counter()
+        print(f"Selenium capture and save screenshot took: {t2-t1}", flush=True)
+
         print(f"Extracting png sequence: {progress_frame*100:.2f}% done", flush=True)
     driver.quit()
