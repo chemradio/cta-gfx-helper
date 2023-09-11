08693bb26f190c22cea5d67c636af0be79f96967
chemradio <chemradio@gmail.com>
2023-09-12 01:11:43 +0600

    further cleanup

--- dispatcher_engine/container_interaction/signal_sender.py
+++ dispatcher_engine/container_interaction/signal_sender.py
@@ -1,7 +1,7 @@
 from container_interaction.screenshoter import signal_to_screenshoter
 from container_interaction.sender import signal_to_sender
 from container_interaction.video_gfx import signal_to_video_gfx
-from db_tortoise.orders_models import Order
+from db_mongo.models.orders import Order
 
 
 async def signal_to_services(order: Order):
@@ -15,7 +15,7 @@ async def signal_to_services(order: Order):
             return await signal_to_video_gfx()
         case "ready_for_send":
             print("Signalling to sender")
-            return await signal_to_sender(order)
+            return await signal_to_sender()
         case _:
             print("no match. signal declined")
             pass
