45a17d5693382190daf1cac934dccd95932ca3a0
chemradio <chemradio@gmail.com>
2023-09-12 01:00:27 +0600

    add sender, remove video_gfx_server

---
+++ sender_engine/container_interaction/edit_order_db.py
@@ -0,0 +1,10 @@
+import requests
+from config import EDIT_ORDER_ENDPOINT
+
+
+def mark_order_sent(sent_order) -> None:
+    r = requests.put(
+        EDIT_ORDER_ENDPOINT,
+        json=sent_order,
+    )
+    return r.json() if r.status_code == 200 else None
---
+++ sender_engine/container_interaction/gather_orders.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+import requests
+
+from config import GET_ONE_ORDER_ENDPOINT
+
+
+def get_ready_to_send_order() -> Optional[list]:
+    try:
+        r = requests.get(
+            GET_ONE_ORDER_ENDPOINT, json={"current_stage": "ready_for_send", "ordered_from": "telegram"}
+        )
+        result = r.json()
+        return result
+    except:
+        return None
