d57ff3fc6abc6b5cdd03148a9d2923fb7c89acfe
chemradio <chemradio@gmail.com>
2023-09-13 18:48:26 +0600

    reworking sender 2

--- sender_engine/send_process/gather_files.py
+++ sender_engine/send_process/gather_files.py
@@ -1,38 +1,22 @@
-from pathlib import Path
-
-import config
+from container_interaction.get_file_storage import fetch_file_from_storage
 
 
 def gather_file_paths(order: dict) -> list:
-    request_type = order.get("request_type")
-    match request_type:
+    match order["request_type"]:
         case "video_auto":
-            return gather_video_auto(order)
+            filetypes = ("video_gfx_name",)
         case "video_files":
-            return gather_video_files(order)
+            filetypes = ("video_gfx_name",)
         case "only_screenshots":
-            return gather_only_screenshots(order)
+            filetypes = ("background_name", "foreground_name")
         case _:
             return []
 
+    files = list()
+    for filetype in filetypes:
+        filename = order.get(filetype)
+        file_bytes = fetch_file_from_storage
+        if file_bytes:
+            files.append((filename, file_bytes))
 
-def gather_video_auto(order: dict) -> list[Path]:
-    video_gfx_name: str = order.get("video_gfx_name")
-    video_gfx_path = config.VIDEO_GFX_FOLDER / str(video_gfx_name)
-    if not video_gfx_path.exists():
-        return None
-    return [video_gfx_path]
-
-
-def gather_video_files(order: dict) -> list[Path]:
-    return gather_video_auto(order)
-
-
-def gather_only_screenshots(order: dict) -> list[Path]:
-    file_paths = list()
-    for filename in ("background_name", "foreground_name"):
-        screenshot_path = config.SCREENSHOTS_FOLDER / str(order[filename])
-        if not screenshot_path.exists():
-            continue
-        file_paths.append(screenshot_path)
-    return file_paths
+    return files
