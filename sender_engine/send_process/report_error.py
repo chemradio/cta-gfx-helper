7232eb40c794d320c830e106308df57f32abff0e
chemradio <chemradio@gmail.com>
2023-09-13 18:52:05 +0600

    reworking sender 3

--- sender_engine/send_process/report_error.py
+++ sender_engine/send_process/report_error.py
@@ -18,3 +18,16 @@ def report_error_raw(order: dict, user_id: int):
         return True
     except:
         return False
+
+    # @classmethod
+    # async def _report_error_to_user(
+    #     cls, order: Order, recipient_telegram_id: int
+    # ) -> None:
+    #     message = f"Произошла ошибка. Пожалуйста, перешли это сообщение администратору.\n\n{dict(order)}"
+    #     kwargs = {"chat_id": recipient_telegram_id, "text": message}
+    #     r = requests.post(config.TELEGRAM_SEND_MESSAGE_API, params=kwargs)
+
+    # @classmethod
+    # async def _send_message(cls, message: str, recipient_telegram_id: int) -> None:
+    #     kwargs = {"chat_id": recipient_telegram_id, "text": message}
+    #     r = requests.post(config.TELEGRAM_SEND_MESSAGE_API, params=kwargs)
