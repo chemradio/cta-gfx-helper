cd12a9017d6da6ff9662d9a68232428068d57289
Timur Timaev <chemradio@gmail.com>
2024-06-30 23:39:02 +0600

    minor fix

--- screenshot_engine/run_tests.py
+++ screenshot_engine/run_tests.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 from src.admin.cookie_generator import generate_cookies
+from src.screenshot_processor import parse_capture_screenshots
 
 COOKIE_FILE_PATH = Path.cwd() / "storage" / "cookie_file.json"
 REMOTE_DRIVER_URL = "http://localhost:4444/wd/hub"
+TARGET_URL = "https://x.com/GeniusGTX/status/1807056930193723595"
 
-TARGET_URL = "https://meduza.io"
 
-generate_cookies(REMOTE_DRIVER_URL, COOKIE_FILE_PATH)
+parse_capture_screenshots(TARGET_URL, REMOTE_DRIVER_URL, COOKIE_FILE_PATH, 2)
