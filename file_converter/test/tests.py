556cfc054be88bdaddcefba5ae56d97b343f8e1e
Timur Timaev <chemradio@gmail.com>
2024-11-16 21:41:47 +0600

    file converter OK

--- file_converter/test/tests.py
+++ file_converter/test/tests.py
@@ -2,13 +2,22 @@ import httpx
 import asyncio
 
 from pathlib import Path
-from workflows.helper_types import mime_map
+
+mime_map = {
+    "application/pdf": "pdf",
+    "image/png": "png",
+    "image/jpeg": "jpg",
+    "audio/wav": "wav",
+    "audio/mpeg": "mp3",
+    "application/msword": "doc",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx",
+}
 
 
 async def submit_file_conversion(file_path: Path):
     async with httpx.AsyncClient() as client:
         return await client.post(
-            "http://127.0.0.1:8000", files={"file": file_path.open("rb")}
+            "http://127.0.0.1:9005", files={"file": file_path.open("rb")}
         )
 
 
@@ -26,23 +35,6 @@ async def seq_test_file_conversion():
             f.write(result.content)
 
 
-async def async_test_file_conversion():
-    # iterate over each file in test/to_convert
-    tasks = []
-    for file_path in Path("test/to_convert").iterdir():
-        print("converting file:", file_path)
-        tasks.append(submit_file_conversion(file_path))
-    results = await asyncio.gather(*tasks)
-    for result in results:
-        # print result file mime type
-        print(result)
-        print(result.headers["Content-Type"])
-
-        converted_extension = mime_map[result.headers["Content-Type"]]
-        with open(f"test/converted/{file_path.stem}.{converted_extension}", "wb") as f:
-            f.write(result.content)
-
-
 async def main():
     from time import perf_counter
 
@@ -50,11 +42,6 @@ async def main():
     await seq_test_file_conversion()
     end = perf_counter()
     print("Sequential time:", end - start)
-
-    start = perf_counter()
-    await async_test_file_conversion()
-    end = perf_counter()
-    print("Async time:", end - start)
 
 
 if __name__ == "__main__":
