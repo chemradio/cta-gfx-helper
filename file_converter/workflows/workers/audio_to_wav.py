76e83dae49f42b99565bc3bdc5908ca4291e2d72
Timur Timaev <TimaevT@kg.corp.rferl.org>
2024-11-18 13:07:09 +0600

    simplify audio conv code. needs testing

--- file_converter/workflows/workers/audio_to_wav.py
+++ file_converter/workflows/workers/audio_to_wav.py
@@ -7,9 +7,6 @@ from pydub import AudioSegment
 
 async def convert_audio_to_wav(file_bytesio: BytesIO) -> BytesIO:
     audio_segment = AudioSegment.from_file(file_bytesio)
-    temp_path = Path.cwd() / f"{secrets.token_hex(8)}.wav"
-    audio_segment.export(temp_path, format="wav")
-    with open(temp_path, "rb") as f:
-        wav_raw = f.read()
-    temp_path.unlink()
-    return BytesIO(wav_raw)
+    audio_bytesio = BytesIO()
+    audio_segment.export(audio_bytesio, format="wav")
+    return audio_bytesio
