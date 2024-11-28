d9d09be36b613a6fad7a4d1908907c73b2fee5b1
Timur Timaev <chemradio@gmail.com>
2024-11-28 13:27:51 +0600

    restructure

--- py_gfxhelper_lib/setup.py
+++ py_gfxhelper_lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gfx_shared_lib",
+    name="py_gfxhelper_lib",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["fastapi==0.115.5", "tinydb==4.8.2"],
