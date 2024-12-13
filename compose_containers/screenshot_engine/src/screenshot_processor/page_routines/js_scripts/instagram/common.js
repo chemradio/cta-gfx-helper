900f9d13592c723c9e044a3c1490f3f05a3cc88a
Timur Timaev <chemradio@gmail.com>
2024-12-13 10:40:29 +0600

    update js scripts - switch to async functions

--- compose_containers/screenshot_engine/src/screenshot_processor/page_routines/js_scripts/instagram/common.js
+++ compose_containers/screenshot_engine/src/screenshot_processor/page_routines/js_scripts/instagram/common.js
@@ -16,30 +16,44 @@ async function getStory() {
     viewStoryButton.click();
   }
 
+  // wait until story is loaded. it's about half a second. but it WILL load
   await delay(1000);
+
   // stop story playback - aria-label="Pause"
   let storyPause = document.querySelector('[aria-label="Pause"]').parentElement;
   if (storyPause) {
+    console.log("pause button found");
+
     storyPause.click();
   }
 
-  //   return document.querySelectorAll('img[draggable="false"]')[1];
   return document.querySelector('[referrerpolicy="origin-when-cross-origin"]');
 }
 
-const parsePost = () => {
-  // try get the post
-  let post = getPost();
-  if (post) {
-    return post;
+const parsePost = async () => {
+  const publicationType = checkPublicationType();
+  if (publicationType === "error") {
+    return null;
+  } else if (publicationType === "post") {
+    return await parsePost();
+  } else if (publicationType === "story") {
+    return await parseStory();
   }
-
-  // try get the story
-  let story = await getStory();
-  return story
 };
 
-const parseProfile = () => {
+const checkPublicationType = () => {
+  if (document.querySelector("article")) return "post";
+  if (
+    document.querySelector('[referrerpolicy="origin-when-cross-origin"]') ||
+    Array.from(document.querySelectorAll('div[role="button"]')).find(
+      (div) => div.innerHTML.trim() === "View story"
+    )
+  )
+    return "story";
+  return "error";
+};
+
+const parseProfile = async () => {
   // find the first class
   let firstClass = document.querySelectorAll(".xvbhtw8");
   let target;
@@ -52,9 +66,13 @@ const parseProfile = () => {
   }
   sidePanel.remove();
 
-  let main = document.querySelector('[role="main"]');
+  const main = document.querySelector('[role="main"]');
   main.parentElement.parentElement.style.margin = "0px";
   main.parentElement.parentElement.parentElement.style.justifyContent =
     "center";
   return document.body;
+};
+
+const extractProfileURL = async () => {
+  const post = await parsePost();
 };
