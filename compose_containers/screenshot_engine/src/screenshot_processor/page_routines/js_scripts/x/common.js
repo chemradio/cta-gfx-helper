39c0da8ca64d6f76af3133883ca35d418d682285
Timur Timaev <chemradio@gmail.com>
2024-12-18 17:35:45 +0600

    .

--- compose_containers/screenshot_engine/src/screenshot_processor/page_routines/js_scripts/x/common.js
+++ compose_containers/screenshot_engine/src/screenshot_processor/page_routines/js_scripts/x/common.js
@@ -1,10 +1,61 @@
-const profileDetectors = [
-    '[aria-label="Home timeline"]',
-    '[data-testid="UserDescription"]',
-    '[aria-label="Profile timelines"]',
-];
+const getPageType = () => {
+    const profileDetectors = [
+        // '[aria-label="Home timeline"]',
+        '[data-testid="UserDescription"]',
+        '[aria-label="Profile timelines"]',
+    ];
 
-const allAria = document.querySelectorAll("[aria-label]");
-for (const aria of allAria) {
-    console.log(aria.getAttribute("aria-label"));
-}
+    const postDetectors = [
+        '[aria-label="Timeline: Conversation"]',
+        'article[role="article"]',
+        '[data-testid="tweet"]',
+    ];
+
+    const isInCategory = (category) => {
+        for (const selector of category) {
+            const element = document.querySelector(selector);
+            if (element) {
+                console.log(element);
+                return true;
+            }
+        }
+        return false;
+    };
+    if (isInCategory(profileDetectors)) {
+        console.log("Page type: PROFILE");
+        return "profile";
+    }
+    if (isInCategory(postDetectors)) {
+        console.log("Page type: POST");
+        return "post";
+    }
+    return "unknown";
+};
+
+const parsePost = async () => {
+    const pageType = getPageType();
+    if (pageType !== "post") return null;
+};
+
+const parseProfile = async () => {
+    if (getPageType() !== "profile") return null;
+    return document.body;
+};
+
+const extractProfileURL = async () => {
+    console.log("extracting profile URL");
+    const pageType = getPageType();
+    if (pageType === "profile") return window.location.href;
+    if (pageType === "post") {
+        const linkSelectors = [
+            '[class="css-175oi2r r-1pi2tsx r-13qz1uu r-o7ynqc r-6416eg r-1ny4l3l r-1loqt21"]',
+            '[class="css-175oi2r r-1wbh5a2 r-dnmrzs r-1ny4l3l r-1loqt21"]',
+        ];
+        for (const selector of linkSelectors) {
+            const link = document.querySelector(selector);
+            if (link) return link.href;
+        }
+    }
+};
+
+await parseProfile();
