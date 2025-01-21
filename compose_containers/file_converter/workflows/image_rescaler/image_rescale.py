c74685fdd8b8b621f05deb0d13341e35914d8282
Timur Timaev <chemradio@gmail.com>
2025-01-22 03:15:41 +0600

    .

--- compose_containers/file_converter/workflows/image_rescaler/image_rescale.py
+++ compose_containers/file_converter/workflows/image_rescaler/image_rescale.py
@@ -1,73 +1,30 @@
 from py_gfxhelper_lib.files.asset_file import AssetFile
-from PIL import Image
+import PIL
 import io
 
 
-async def image_rescale(
-     max_width: int | float | None, max_height: int | float | None
+def rescale_image(
+    image_file: AssetFile,
+    max_width: int | float | None = None,
+    max_height: int | float | None = None,
 ) -> AssetFile:
     if not max_width and not max_height:
         raise ValueError("At least one of max_width or max_height must be provided")
-    # Open the image file
-    image = 
 
-    image_aspect_ratio = image.width / image.height
-    if max_width and max_height:
-        target_aspect_ratio = max_width / max_height
-        if image_aspect_ratio > target_aspect_ratio:
-            new_width = max_width
-            new_height = int(max_width / image_aspect_ratio)
-        else:
-            new_height = max_height
-            new_width = int(max_height * image_aspect_ratio)
-    elif max_width:
-        ...
-    # if image.width > max_width or image.height > max_height:
-    #     if aspect_ratio > 1:
-    #         new_width = min(image.width, max_width)
-    #         new_height = int(new_width / aspect_ratio)
-    #     else:
-    #         new_height = min(image.height, max_height)
-    #         new_width = int(new_height * aspect_ratio)
-    # else:
-    #     new_width, new_height = image.width, image.height
-
-    # # Resize the image
-    # resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
-
-    # # Save the resized image back to the AssetFile
-    # output = io.BytesIO()
-    # resized_image.save(output, format=image.format)
-    # return AssetFile(bytes_or_bytesio=output, extension=image_file.extension)
-    ...
-
-
-
-def rescale_image(image_file: AssetFile, max_width:int|float|None=None, max_height:int|float|None=None):
-    with Image.open(image_file.bytesio) as original_image:
+    with PIL.Image.open(image_file.bytesio) as original_image:
         original_width, original_height = original_image.size
+        if not max_width:
+            max_width = original_width
+
+        if not max_height:
+            max_height = original_height
+
+        original_image.thumbnail((max_width, max_height), PIL.Image.ANTIALIAS)
+
         output_bytesio = io.BytesIO()
-        
-        # Check if resizing is necessary
-        if max_width and max_height:
-            if max_width < original_width and max_height < original_height:
-                original_image.thumbnail((max_width, max_height))
-            original_image.save(output_bytesio, format=original_image.format)
+        original_image.save(output_bytesio, format="PNG")
 
-        elif max_width:
-            if max_width < original_width:
-                scale_factor = max_width / original_width
-                new_height = int(original_height * scale_factor)
-                original_image = original_image.resize((max_width, new_height), Image.ANTIALIAS)
-            original_image.save(output_bytesio, format=original_image.format)
-
-        elif max_height:
-            if max_height >= original_height:
-                original_image.save(output_bytesio, format=original_image.format)
-                return
-            scale_factor = max_height / original_height
-            new_width = int(original_width * scale_factor)
-            original_image = original_image.resize((new_width, max_height), Image.ANTIALIAS)
-        
-        original_image.thumbnail((max_width, max_height))
-        original_image.save(output_path)
+        return AssetFile(
+            bytes_or_bytesio=output_bytesio,
+            extension="png",
+        )
