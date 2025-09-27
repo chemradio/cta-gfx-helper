5c9ce8310d6521fb0e8a2fa797033c036beef99d
tim home <chemradio@gmail.com>
2025-09-27 14:47:16 +0600

    update file_convert.py

--- compose_containers/file_converter/workflows/converters/file_convert.py
+++ compose_containers/file_converter/workflows/converters/file_convert.py
@@ -7,6 +7,14 @@ from .workers import (
 from py_gfxhelper_lib.files import AssetFile
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
 async def convert_unsupported_file(
     file: AssetFile,
 ) -> AssetFile:
@@ -14,20 +22,34 @@ async def convert_unsupported_file(
     IMAGE_EXTENSIONS = ("png", "jpg", "jpeg")
     WORD_EXTENSIONS = ("doc", "docx")
 
+    logger.info(f"Attempting conversion for file: {file.extension}")
+
     if file.extension in AUDIO_EXTENSIONS:
+        logger.info("Detected audio file, converting to WAV format")
         converted_bytesio = await convert_audio_to_wav(file.bytesio)
         converted_mime_type = "audio/wav"
 
     if file.extension in IMAGE_EXTENSIONS:
+        logger.info("Detected image file, converting to PNG format")
         converted_bytesio = await convert_image_to_png(file.bytesio)
         converted_mime_type = "image/png"
 
     if file.extension in WORD_EXTENSIONS:
+        logger.info("Detected Word file, converting to PNG format")
         converted_bytesio = await convert_word_to_png(file.bytesio)
         converted_mime_type = "image/png"
 
     if file.extension == "pdf":
+        logger.info("Detected PDF file, converting to PNG format")
         converted_bytesio = await convert_pdf_to_png(file.bytesio)
         converted_mime_type = "image/png"
+
+    logger.info(
+        f"File conversion successful: {file.extension} to {converted_mime_type}"
+    )
+    logger.info(
+        f"Converted file size in MB is: {converted_bytesio.getbuffer().nbytes / (1024 * 1024)}"
+    )
+    converted_bytesio.seek(0)
 
     return AssetFile(bytes_or_bytesio=converted_bytesio, mime_type=converted_mime_type)
