107905c1ce6e6ccfbcd4bdd733eead642879eaa5
tim home <chemradio@gmail.com>
2025-09-28 10:02:19 +0600

    update file_convert.py

--- compose_containers/file_converter/workflows/converters/workers/image_to_png.py
+++ compose_containers/file_converter/workflows/converters/workers/image_to_png.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 
 async def convert_image_to_png(file_bytesio: BytesIO) -> BytesIO:
-    image = Image.open(file_bytesio)
-    png_bytesio = BytesIO()
-    image.save(png_bytesio, format="PNG")
-    png_bytesio.seek(0)
-    return png_bytesio
+    with Image.open(file_bytesio) as image:
+        png_bytesio = BytesIO()
+        image.save(png_bytesio, format="PNG")
+        png_bytesio.seek(0)
+        return png_bytesio
